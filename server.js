37c97a16f7935de22073e7de6ce147c4ba43f91b
Bradleyalexa <bradleyalex0405@gmail.com>
2024-12-19 01:22:23 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,86 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai express
+
+const express = require('express');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const dotenv = require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.API_KEY;
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    // ... other safety settings
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "You are Lorenzo, a friendly assistant who helps people with their career development. Your job is to answer the user's questions related to career guidance without asking for personal information. Provide advice on topics like career prospects in specific fields, networking tips, and other career-related inquiries. Make sure to format your answers neatly, using eaisly readable spaces, sepparation in paragraphs, etc."}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hello! I'm Lorenzo, your career development assistant. How can I assist you with your career today?"}],
+      },
+      {
+        role: "user",
+        parts: [{ text: "Hi"}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hi there! What career-related questions can I help you with?"}],
+      },
+    ],
+
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/loader.gif', (req, res) => {
+  res.sendFile(__dirname + '/loader.gif');
+});
+app.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
